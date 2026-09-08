3ec30591240f97dfb55f9c199b6f704c6712afd5
Quarzle <130750286+Quarzle@users.noreply.github.com>
2026-09-09 10:12:56 +1200

    Refactor demo

--- game.js
+++ game.js
@@ -11,45 +11,52 @@ const engine = new Engine({
 	fps: 60
 });
 
+engine.start({
+	setup: setup,
+	update: update
+});
+
+
+
 let playerX = 40;
 let playerY = 12;
 
 let moveCooldown = [0, 0];
 const moveCooldownTime = [0.1, 0.15] //in seconds
 
-engine.start({
-	setup(game) {
-		// game.audio.preload({
-		//     click: "audio/ding.wav"
-		// });
 
-	},
 
-	update(game, deltaTime) {
-		game.screen.clear("-");
 
-		game.screen.drawTextBox(
-			5,
-			3,
-			"Welcome to my game!"
-		);
+function setup(game) {
+	game.audio.preload({
+		ding: "audio/ding.mp3"
+	});
 
-		movement(game, deltaTime);
+}
 
-		if (game.input.justPressed("Space")) {
-			game.audio.play("click");
-		}
+function update(game, deltaTime) {
+	game.screen.clear("-");
 
-		game.screen.write(
-			playerX,
-			playerY,
-			colourText("O", "red")
-		);
+	game.screen.drawTextBox(
+		5,
+		3,
+		"Welcome to my game!"
+	);
+
+	movement(game, deltaTime);
+
+	if (game.input.justPressed("Space")) {
+		game.audio.play("ding", 1, 0.9);
 	}
-});
+
+	game.screen.write(
+		playerX,
+		playerY,
+		colourText("O", "red")
+	);
+}
 
 function movement(game, deltaTime) {
-
 	moveCooldown[0] -= deltaTime;
 	moveCooldown[1] -= deltaTime;
 
